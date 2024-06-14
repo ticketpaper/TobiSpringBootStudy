9f7fa8cb874508d2005e958a00956b33ce7c57ee
ticketpaper <dlwhdvy369@naver.com>
2024-06-14 16:48:30 +0900

    first commit

---
+++ src/main/java/com/study/tobispringboot/TobiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.study.tobispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobiSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/tobispringboot/TobiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.tobispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
